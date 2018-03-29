7f78ed1fbc86a662d30437ed6fbb83943f03d972
takeru <vitual.money.tkr@gmail.com>
2018-03-29 18:03:06 +0900

    waruguchi

--- linebot/spotify_search.py
+++ linebot/spotify_search.py
@@ -1,6 +1,8 @@
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from pykakasi import kakasi
+import re
 
 class taketify(spotipy.Spotify):
 
@@ -21,8 +23,11 @@ class taketify(spotipy.Spotify):
         item = result["artists"]["items"]
         if len(item) > 0:
             artist = item[0]
-            return artist['images'][0]['url']
+            image_url = artist['images'][0]['url']
+        else:
+            image_url = False
 
+        return image_url
 
     def spotify_sample_audio(self, name):
 
@@ -34,11 +39,38 @@ class taketify(spotipy.Spotify):
             af = item[0]
             theid = af["id"]
             results = spotify.artist_top_tracks(theid)
-            items = results["tracks"][:3]
+            items = results["tracks"][:5]
             a = ""
             for i in reversed(items):
                 if i["preview_url"]:
                     a = i["preview_url"]
             if a == "":
-                a = "Sorry, no contents has been detected.."
+                a = "そのアーティストの曲はないみたいだなぁ。。。"
+        else:
+            a = "呼んだ？！\n あ、呼んでない？ごめんごめん。"
         return a
+
+
+class Goodbye(pykakasi.kakasi):
+
+    def kakasi_set(self, target):
+        target = str(target)
+        kaka = kakasi()
+        kaka.setMode('H', 'a')
+        kaka.setMode('K', 'a')
+        kaka.setMode('J', 'a')
+        conv = kaka.getConverter()
+        text = conv.do(target)
+        return text
+
+    def check(self, chat):
+        waruguchi = [
+        'gm', 'bai', 'bye', 'www', 'sine', 'gomi',
+         'kunna', 'deteke', 'kikasaru', 'sayonara',
+         'sarusine', 'tukaenai'
+         ]
+         text_sent = self.kakasi_set(chat)
+         flag = False
+         for i in waruguchi:
+             if re.search(i, text_sent):
+                 flag = True
--- linebot/views.py
+++ linebot/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 import json
 import requests
-from .spotify_search import taketify
+from .spotify_search import taketify, Goodbye
 import re
 import os
+from linebot import LineBotApi
+from linebot.exceptions import LineBotApiError
+from pykakasi import kakasi
 
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 ACCESS_TOKEN = os.environ["LINE_TOKEN"]
+
 
 HEADER = {
     "Content-Type": "application/json",
@@ -20,16 +24,29 @@ def reply_image(reply_token, text, name):
     artist = name
     rep = taketify()
     reply = rep.spotify_image(artist)
-    payload = {
-          "replyToken":reply_token,
-          "messages":[
-                {
-                    "type":"image",
-                    "originalContentUrl": reply,
-                    "previewImageUrl": reply,
-                }
-            ]
-    }
+
+    if reply == False:
+        reply = "ごめんなさい、その人は知らないなぁ。。"
+        payload = {
+              "replyToken":reply_token,
+              "messages":[
+                    {
+                        "type":"text",
+                        "text":reply
+                    }
+                ]
+        }
+    else:
+        payload = {
+              "replyToken":reply_token,
+              "messages":[
+                    {
+                        "type":"image",
+                        "originalContentUrl": reply,
+                        "previewImageUrl": reply,
+                    }
+                ]
+        }
 
     requests.post(REPLY_ENDPOINT, headers=HEADER, data=json.dumps(payload))
     return reply
@@ -55,12 +72,41 @@ def reply_sample(reply_token, text, name):
     requests.post(REPLY_ENDPOINT, headers=HEADER, data=json.dumps(payload))
     return reply
 
+def leave_groups(id):
+    line_bot_api = LineBotApi(HEADER["Authorization"])
+    try:
+        line_bot_api.leave_group(id)
+    except LineBotApiError as e:
+        break
+
+def leave_rooms(id):
+    line_bot_api = LineBotApi(HEADER["Authorization"])
+    try:
+        line_bot_api.leave_room(id)
+    except LineBotApiError as e:
+        break
+
 def callback(request):
     reply = ""
     request_json = json.loads(request.body.decode('utf-8'))
     for e in request_json['events']:
         reply_token = e['replyToken']
         message_type = e['message']['type']
+        if e["source"]["type"] != "user" and message_type == "text":
+            check = Goodbye()
+            text = e["message"]["text"]
+            judge = check.check(text)
+
+            if judge == True and e["source"]["type"] == "group":
+                group_id = e["source"]["groupId"]
+                leave_groups(group_id)
+
+            if judge == True and e["source"]["type"] == "room":
+                room_id = e["source"]["roomId"]
+                leave_group(room_id)
+
+            else:
+                continue
 
         if message_type == 'text':
             text = e['message']['text']
@@ -70,4 +116,5 @@ def callback(request):
                 reply += reply_image(reply_token, image, image)
             else:
                 reply += reply_sample(reply_token, text, text)
+
     return HttpResponse(reply)
